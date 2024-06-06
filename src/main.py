f62557e7d584213de685f651cd8300eee8c309bf
Eslem Karakaş <eslem.karakas.tr@gmail.com>
2024-06-06 10:18:11 +0300

    feat: Call training and predicting pipeline for prod.

---
+++ src/main.py
@@ -0,0 +1,17 @@
+from learner.train import train_model, load_and_predict
+
+class C:
+    TRAIN = False
+
+def main(f1, f2):
+    # train the model if needed, it is unnneeded in prod env
+    if C.TRAIN: 
+        train_model()
+    
+    # predict r1, r2, r3, r4, r5 values on given f1 and f2 values by using trained models
+    load_and_predict(f1, f2) # return a list that contains 5 float numbers
+    
+if __name__ == '__main__':
+    f1 = None
+    f2 = None
+    main(f1, f2)
